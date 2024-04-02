d216ebf31396d23080a53ee6cfa9ea62e4560e5b
AzizDaagi <daagi.aziz09403@gmail.com>
2024-04-02 11:39:48 +0100

    weather

---
+++ src/components/Weather.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import "./weather.css"
+
+function Weather() {
+  const apiKey = "8945288390951ce81c59e947cdcb9611";
+  const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
+  const searchBox = document.querySelector(".searchBox");
+  const searchBtn = document.querySelector(".searchBtn");
+  const temp = document.querySelector(".temp");
+  const city = document.querySelector(".city");
+
+  async function fetchData() {
+    const response = await fetch(
+      apiUrl + searchBox.value + "&units=metric&appid=" + apiKey
+    );
+    const data = await response.json();
+    console.log(data);
+    city.innerHTML = data.name;
+    temp.innerHTML = Math.floor(data.main.temp) + " °c";
+  }
+  searchBtn.addEventListener("click", () => {
+    fetchData();
+  });
+  searchBox.addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
+      searchBtn.click();
+    }
+  });
+
+  return (
+    <div>
+      <div className="search">
+            <input className="searchBox" type="text" placeholder="Enter city name" spellcheck="false"/>
+            <button className="searchBtn"><img className="icon" src={"https://static-00.iconduck.com/assets.00/search-icon-2048x2048-cmujl7en.png"} /></button>
+        </div> 
+        <div className="weather">
+            <img 
+            className="image" src={"https://cdn-icons-png.flaticon.com/512/3161/3161249.png"}/>
+            <h1 className="temp">22° C</h1>
+            <h2 className="city">Tunis</h2>
+        </div>
+    </div>
+  )
+}
+
+export default Weather
